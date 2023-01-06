db9ed216524c2c02a72709cdaf3d0d7730deb3e9
waqorg <25316657+waqorg@users.noreply.github.com>
2023-01-06 21:29:36 +0000

    check arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
 
 # Print the number of arguments
-    print("{:d} arguments:", len(sys.argv) - 1)
+    print(len(sys.argv) - 1, "arguments:")
 
 # Print the list of arguments with numbers
     for i, arg in enumerate(sys.argv[1:]):
