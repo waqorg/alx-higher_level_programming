e716751490aecfdb2148d2e0321d14dd81e02bf1
waqorg <25316657+waqorg@users.noreply.github.com>
2023-01-06 20:37:09 +0000

    add function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-
-from add_0 import add
-
-a = 1
-b = 2
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+f __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, result))
