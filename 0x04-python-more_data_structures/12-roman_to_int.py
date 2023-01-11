d612dbf38209d5767e6015630e26cce6fd163c4b
waqorg <25316657+waqorg@users.noreply.github.com>
2023-01-11 16:15:52 +0000

    update roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) != str:
+        return 0
+    roman_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result = 0
+    for i in range(len(roman_string)):
+        curr_val = roman_map[roman_string[i]]
+        if i < len(roman_string) - 1:
+            next_val = roman_map[roman_string[i + 1]]
+            if curr_val < next_val:
+                result -= curr_val
+            else:
+                result += curr_val
+        else:
+            result += curr_val
+    return result
