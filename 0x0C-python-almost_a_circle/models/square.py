76ccfa3f893172cf4c7dcf1d77648918d7335c48
waqorg <25316657+waqorg@users.noreply.github.com>
2023-02-13 16:48:46 +0000

    tests model update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,7 +6,7 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Module Representation of Square
+    """Module Representation of the Square
 """
 
     def __init__(self, size, x=0, y=0, id=None):
